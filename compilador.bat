849097481824b0d8945ac50aa87aeb642c892c44
Carlos Ivan Solis Donoso <solisdonoso@gmail.com>
2022-07-23 21:55:02 -0500

    prueba

--- compilador.bat
+++ compilador.bat
@@ -1,7 +1,7 @@
 SET CLASSPATH=C:\WINDOWS\LIB\*;
-"C:\Program Files\Java\jdk-18.0.1\bin\javac" windowMed.java
-"C:\Program Files\Java\jdk-18.0.1\bin\javac" windowPac.java
-"C:\Program Files\Java\jdk-18.0.1\bin\javac" Main.java
+"C:\Program Files\Java\jdk-18.0.2\bin\javac" windowMed.java
+"C:\Program Files\Java\jdk-18.0.2\bin\javac" windowPac.java
+"C:\Program Files\Java\jdk-18.0.2\bin\javac" Main.java
 pause
-"C:\Program Files\Java\jdk-18.0.1\bin\java" Main
+"C:\Program Files\Java\jdk-18.0.2\bin\java" Main
 pause
